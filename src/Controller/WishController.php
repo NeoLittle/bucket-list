56a8ee2e9b5755939440b42c59d75d41eea759e0
NeoLittle <mornet.remy@gmail.com>
2022-10-10 17:19:14 +0200

    début affichage et faire les TODO

--- src/Controller/WishController.php
+++ src/Controller/WishController.php
@@ -2,6 +2,9 @@
 
 namespace App\Controller;
 
+use App\Entity\Wish;
+use App\Form\WishType;
+use http\Env\Request;
 use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Routing\Annotation\Route;
@@ -19,5 +22,12 @@ class WishController extends AbstractController
      */
     public function details(int $id):Response{
         return $this->render('wish/details.html.twig');
+    }
+    public function create(Request $request):Response{
+        $wish = new Wish();
+        $wishform = $this->createForm(WishType::class, $wish);
+        //Todo: Faire la page Create (pas le mod minecraft)
+
+        return $this->render('');
     }
 }
