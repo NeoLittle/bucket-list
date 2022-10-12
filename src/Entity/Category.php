e2fa6c6324496cafc838535bb494dd7c707ea892
NeoLittle <mornet.remy@gmail.com>
2022-10-12 14:01:17 +0200

    Deuxième entité ajoutée Deuxième BDD crée Relation entre les deux

---
+++ src/Entity/Category.php
@@ -0,0 +1,59 @@
+<?php
+
+namespace App\Entity;
+
+use App\Repository\CategoryRepository;
+use Doctrine\ORM\Mapping as ORM;
+
+/**
+ * @ORM\Entity(repositoryClass=CategoryRepository::class)
+ */
+class Category
+{
+    /**
+     * @ORM\Id
+     * @ORM\GeneratedValue
+     * @ORM\Column(type="integer")
+     */
+    private $id;
+
+    /**
+     * @ORM\Column(type="string", length=50)
+     */
+    private $name;
+
+    /**
+     * @ORM\OneToOne(targetEntity=Wish::class, inversedBy="category", cascade={"persist", "remove"})
+     * @ORM\JoinColumn(nullable=false)
+     */
+    private $wish;
+
+    public function getId(): ?int
+    {
+        return $this->id;
+    }
+
+    public function getName(): ?string
+    {
+        return $this->name;
+    }
+
+    public function setName(string $name): self
+    {
+        $this->name = $name;
+
+        return $this;
+    }
+
+    public function getWish(): ?Wish
+    {
+        return $this->wish;
+    }
+
+    public function setWish(Wish $wish): self
+    {
+        $this->wish = $wish;
+
+        return $this;
+    }
+}
